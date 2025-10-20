a9957ffe920b5a843d6c3fc5915a885d351a235f
RyanNNguyen05 <rnguyen1@usf.edu>
2025-10-19 22:55:22 -0400

    first commit

---
+++ cp_2.js
@@ -0,0 +1,35 @@
+function fetchProductsThen() {
+    fetch("https://www.course-api.com/javascript-store-products")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((products) => {
+        products.forEach((product) => {
+            console.log(product.name);
+        });
+    })
+    .catch((error) => {
+        console.error("Error fetching products:", error);
+    });
+}
+fetchProductsThen();
+
+function displayProducts(products) {
+    const container = document.getElementById("product-container");
+    container.innerHTML="";
+    products.forEach((product, index) => {
+        const furnitureNames = ["Couch", "Coffee Table", "Dresser", "Bed Frame"];
+        const name = furnitureNames[index % furnitureNames.length];
+        const card = document.createElement("div");
+        card.className = "product-card"
+        card.innerHTML = `
+        <h3>${name}</h3>
+        <p>Price: $${(product.price / 100).toFixed(2)}</p>
+        <img src="${product.image}" alt="${name}" />
+        `;
+        container.appendChild(card);
+    });
+}
